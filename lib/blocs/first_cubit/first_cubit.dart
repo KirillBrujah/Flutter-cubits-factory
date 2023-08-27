56320d6fe7aa31c223f7723f3c014a7897f7c44d
kirillbrujah <kirillbrujah@gmail.com>
2023-08-27 23:19:57 +0200

    Implement load function in first cubit

--- lib/blocs/first_cubit/first_cubit.dart
+++ lib/blocs/first_cubit/first_cubit.dart
@@ -7,5 +7,19 @@ class FirstCubit extends DataCubit<DataState<FirstModel>> {
   @override
   Future<void> load() async {
     emit(const DataState.initial());
+
+    try {
+      /// Mocking async load
+      await Future.delayed(const Duration(milliseconds: 500));
+
+      final data = List.generate(
+        15,
+        (index) => FirstModel(id: index, name: 'Item $index'),
+      );
+
+      emit(DataState.data(data));
+    } catch (e) {
+      emit(DataState.error(e.toString()));
+    }
   }
 }
